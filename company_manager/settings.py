2f28c50496d3803e7e8d68e125174832873faf0b
Richard <lordrichado@gmail.com>
2022-01-09 21:08:14 +0100

    edited a setting

--- company_manager/settings.py
+++ company_manager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-88$h1y*ws_nt3k6q$lzvi868*gb$+ywv2hcu1czloi83$-9u2#'
+SECRET_KEY = 'django-insecure-88$h1y*ws_nt3k6q$lzvi868*gb$+ywv2hcu1czloi83$-9u2#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
